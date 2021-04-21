5ac1807c0119d0921484eeb879a057a131714c08
Berkan Şahin <berkan.sahin@ug.bilkent.edu.tr>
2021-04-21 10:16:37 +0300

    Added BadExampleException

---
+++ src/main/java/me/labconnect/webapp/unittest/BadExampleException.java
@@ -0,0 +1,35 @@
+package me.labconnect.webapp.unittest;
+
+/**
+ * An exception thrown when the example implementation supplied by the
+ * instructor fails the unit test.
+ * 
+ * @author Berkan Şahin
+ * @version 21.04.2021
+ */
+public class BadExampleException extends Exception {
+
+    // Variables
+    TestResult result;
+
+    // Constructors
+
+    /**
+     * Create a new BadExampleException instance
+     * @param result The result of the failed test
+     */
+    public BadExampleException(TestResult result) {
+        this.result = result;
+    }
+
+    // Methods
+
+    /**
+     * Return the result of the failed test
+     * 
+     * @return the result of the failed test
+     */
+    public TestResult getResult() {
+        return result;
+    }
+}
