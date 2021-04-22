fc376e81137f1707d3123cc7d21aa51495eba2d5
Berkan Şahin <berkan.sahin@ug.bilkent.edu.tr>
2021-04-22 11:12:27 +0300

    Clarify Tester documentation

--- src/main/java/me/labconnect/webapp/testing/Tester.java
+++ src/main/java/me/labconnect/webapp/testing/Tester.java
@@ -7,14 +7,14 @@ import java.nio.file.Path;
  * Tester interface for classes that are testing
  * distinct elements of an submission
  * @author Borga Haktan Bilen
- * @version 21/04/2021
+ * @version 21.04.2021
  * @throws IOException If an I/O error occurs while processing the submission
  */
 public interface Tester {
     /**
      * Runs distinct test for testing discrete elements
      * of an submission
-     * @param submission The path for the submission file
+     * @param submission The *absolute* path of the submission directory (usually src/)
      * @return TestResult object
      */
     TestResult runTest( Path submission ) throws IOException;
