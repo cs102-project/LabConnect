26307031da6dd5a230aefce23efe87ebac96a247
Berkan Şahin <berkan.sahin@ug.bilkent.edu.tr>
2021-04-22 21:59:05 +0300

    Fix "instution" typo in user classes

--- src/main/java/me/labconnect/webapp/models/User.java
+++ src/main/java/me/labconnect/webapp/models/User.java
@@ -3,36 +3,38 @@ package me.labconnect.webapp.models;
 import org.springframework.data.annotation.Id;
 
 /**
- * User superclass for general properties
- * and methods
+ * User superclass for general properties and methods
+ * 
  * @author Borga Haktan Bilen
- * @version 18/04/2021 
+ * @author Berkan Şahin
+ * @version 22.04.2021
  */
 public abstract class User {
-    
+
     // Properties
     @Id
-    private Long uuid;     // Maybe mongo equivalent of sequance generator needed?
-    private long id;
+    private String objectID;
+    private long userID;
     private String name;
     private String department;
 
     // Constructor
     /**
      * Initializes the shared properties
-     * @param name The name of the user
-     * @param instutionId The unique instutionId of User
-     * @param department The department of the User
+     * 
+     * @param name          The name of the user
+     * @param institutionId The unique institutionId of User
+     * @param department    The department of the User
      */
-    public User( String name, long instutionId, String department ) {
+    public User(String name, long institutionId, String department) {
         this.name = name;
-        id = instutionId;
+        userID = institutionId;
         this.department = department;
     }
 
     // Methods
-    public long getId() {
-        return id;
+    public long getUserId() {
+        return userID;
     }
 
     public String getName() {
@@ -43,9 +45,9 @@ public abstract class User {
         return department;
     }
 
-    /* Need an online status checking method.
-     * TO-DO after the implementation of LiveSessionManager
-     * class. Additionally, for online status subclasses of this
+    /*
+     * Need an online status checking method. TO-DO after the implementation of
+     * LiveSessionManager class. Additionally, for online status subclasses of this
      * class might need a small modification also
-    */
+     */
 }
