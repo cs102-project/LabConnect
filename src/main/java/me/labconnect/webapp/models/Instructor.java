794a5ac40ea01b4e64a06cb4979d9edfe45b656a
Berkan Şahin <berkan.sahin@ug.bilkent.edu.tr>
2021-04-27 21:56:14 +0300

    How could we forget this lol

---
+++ src/main/java/me/labconnect/webapp/models/Instructor.java
@@ -0,0 +1,42 @@
+package me.labconnect.webapp.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * An instructor, which is the user that creates assignments, starts live
+ * sessions and uploads tests
+ * 
+ * @author Berkan Şahin
+ * @version 27.04.2021
+ */
+@Document(collection = "users")
+public class Instructor extends User {
+
+    // Variables
+    private int[] sections;
+
+    /**
+     * Create a new Instructor instance
+     * 
+     * @param name          The name of the instructor
+     * @param institutionId The unique institution ID of the instructor
+     * @param department    The department of the instructor
+     * @param sections      The sections taught by this instructor
+     */
+    public Instructor(String name, long institutionId, String department, int[] sections) {
+        super(name, institutionId, department);
+        this.sections = sections;
+    }
+
+    // Methods
+
+    /**
+     * Returns the sections taught by this instructor
+     * 
+     * @return the sections taught by this instructor
+     */
+    public int[] getSections() {
+        return sections;
+    }
+
+}
