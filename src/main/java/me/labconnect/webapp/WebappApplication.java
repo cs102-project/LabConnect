ae8b1232a31f93986bb8d1167a75b74a420803d1
Ved <dev@vedat.xyz>
2021-04-29 01:19:36 +0300

    Configured Spring-side of SPA routing

--- src/main/java/me/labconnect/webapp/WebappApplication.java
+++ src/main/java/me/labconnect/webapp/WebappApplication.java
@@ -1,27 +1,13 @@
 package me.labconnect.webapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class WebappApplication {
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private AssignmentRepository assignmentRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
-
-	@GetMapping("/api/dummy")
-	public String dummyWorkflow() {
-		// TODO drop everything, add instructor, add assignment etc.
-		return "Success!\n";
-	}
+	
 }
