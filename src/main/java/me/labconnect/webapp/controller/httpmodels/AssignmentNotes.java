16bcfd376b57b97158b1a215394566b737c14bbe
Ved <dev@vedat.xyz>
2021-05-03 22:59:09 +0300

    Left-overs from last commit

---
+++ src/main/java/me/labconnect/webapp/controller/httpmodels/AssignmentNotes.java
@@ -0,0 +1,68 @@
+package me.labconnect.webapp.controller.httpmodels;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+/**
+ * HTTP model class for {@code Note} objects
+ *
+ * @author Vedat Eren Arican
+ * @author Berk Çakar
+ * @version 03.05.2021
+ */
+public class AssignmentNotes {
+
+    private String assignmentTitle;
+    private ObjectId assignmentId;
+    private List<AttemptNote> attemptNotes;
+
+    /**
+     * Default constructor for the {@code Note} class takes content of the note as parameter
+     *
+     * @param content String content of the {@code Note} object
+     */
+    public AssignmentNotes(String assignmentTitle, ObjectId assignmentId, List<AttemptNote> attemptNotes) {
+        this.assignmentTitle = assignmentTitle;
+        this.assignmentId = assignmentId;
+        this.attemptNotes = attemptNotes;
+    }
+
+    /**
+     * Gets the content of the {@code Note} object
+     *
+     * @return String content of the {@code Note} object
+     */
+    public String getAssignmentTitle() {
+        return assignmentTitle;
+    }
+    
+    public List<AttemptNote> getAttemptNotes() {
+        return attemptNotes;
+    }
+    
+    public ObjectId getAssignmentId() {
+        return assignmentId;
+    }
+    
+    public static class AttemptNote {
+        
+        private String note;
+        private int attempt;
+        
+        public AttemptNote(String note, int attempt) {
+            this.note = note;
+            this.attempt = attempt;
+        }
+        
+        public String getNote() {
+            return note;
+        }
+        
+        public int getAttempt() {
+            return attempt;
+        }
+        
+    }
+    
+}
