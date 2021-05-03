694088d589f0c383bf4a6192496a3d25b4b27c52
Ved <dev@vedat.xyz>
2021-05-03 17:37:42 +0300

    Added possible solution to ObjectId serialization

--- src/main/java/me/labconnect/webapp/WebappConfiguration.java
+++ src/main/java/me/labconnect/webapp/WebappConfiguration.java
@@ -1,7 +1,15 @@
 package me.labconnect.webapp;
 
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,7 +22,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebappConfiguration implements WebMvcConfigurer {
-
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        
+        builder.serializerByType(ObjectId.class, new ToStringSerializer());
+        
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(builder.build());
+        
+        converters.add(converter);
+        
+    }
+    
     /**
      * Adds resource handlers
      * 
