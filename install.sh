003db7c34397fe0ccaf5b9701b28294f2176c409
Berkan Şahin <berkan.sahin@ug.bilkent.edu.tr>
2021-05-04 10:54:58 +0300

    Support more OSes

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-# TODO check distro, don't assume ubuntu/debian
+# This is a shell script that configures and deploys LabConnect for:
+# * Fedora 33 or later
+# * Ubuntu 20.04 or later
+# Any recent version of macOS (might not be complete)
+# Since this program relies on node.js and Docker, it is not recommended to run it on Windows
+# Please use a virtual machine or any other means of running GNU/Linux or macOS if that is the case
+
+which apt >/dev/null 2>&1 && PKGMAN=apt || which dnf >/dev/null 2>&1 && PKGMAN=dnf || which brew >/dev/null 2>&1 && PKGMAN=brew || echo "OS not supported" && exit 1
 echo "Updating the system, just in case"
-sudo apt update && sudo apt -y upgrade
+[ $PKGMAN = "apt" ] && sudo apt update && sudo apt -y upgrade
+[ $PKGMAN = "dnf" ] && sudo dnf -y update
+[ $PKGMAN = "brew" ] && brew update
 printf "\n\n"
 printf "Packages to install\tTheir purpose:\n"
 printf "**************************************\n"
@@ -10,7 +19,9 @@ printf "Docker\nDocker-compose\t\tFor building and deploying the container setup
 printf "npm\nNode.js\t\t\tFor building the frontend\n"
 printf "OpenJDK 11\t\tFor building the backend\n"
 printf "\n\n"
-sudo apt -y install docker docker-compose npm nodejs openjdk-11-jdk
+[ $PKGMAN = "apt" ] && sudo apt -y install docker docker-compose npm nodejs openjdk-11-jdk
+[ $PKGMAN = "dnf" ] && sudo dnf -y in docker docker-compose nodejs npm java-11-openjdk-devel
+[ $PKGMAN = "brew" ] && brew install docker docker-compose npm nodejs openjdk-11-jdk
 printf "\n\n"
 echo "Now building the frontend, might take some time during the first run because of dependencies"
 printf "\n\n"
@@ -22,7 +33,7 @@ printf "\n\n"
 printf "\n\n"
 echo "Now deploying the container composition, this takes some time and bandwidth the first time"
 printf "\n\n"
-sudo systemctl start docker
+[ $PKGMAN != "brew" ] && sudo systemctl start docker
 sudo docker-compose up --force-recreate --build -V -d
 printf "\n\n"
 echo "You're all set!"
