95ae5f1621b02f517ed7f2707684471ef16f0298
Ved <dev@vedat.xyz>
2021-05-05 05:51:40 +0300

    Fixing bugs

--- src/main/react-client/src/components/CourseStatus.tsx
+++ src/main/react-client/src/components/CourseStatus.tsx
@@ -7,6 +7,7 @@ function CourseStatus(): JSX.Element {
     
     const [userSelf, setUserSelf] = useState<IUserSelf>();
     const [assignments, setAssignments] = useState<IAssignment[]>();
+    const [stats, setStats] = useState({ complete: 0, total: 1 });
     
     useEffect(() => {
         APITools.getUserSelf().then((response) => {
@@ -14,6 +15,10 @@ function CourseStatus(): JSX.Element {
         });
         APITools.getAllAssignments().then((response) => {
             setAssignments(response);
+            setStats({
+                complete: assignments?.filter((el) => APITools.helpers.isAssignmentComplete(el))?.length || 0,
+                total: assignments?.length || 1
+            });
         });
     }, []);
     
@@ -31,14 +36,14 @@ function CourseStatus(): JSX.Element {
             <div id="dashboard-course-progress">
                 <div id="course-progress-text">
                     <p>
-                        Course Progress: {assignments?.filter((el) => APITools.helpers.isAssignmentComplete(el)).length} <span>/ {assignments?.length} assignments</span>
+                        Course Progress: {stats.complete} <span>/ {stats.total} assignments</span>
                     </p>
                     <p>
                         Next Deadline: <span>4 days left!</span>
                     </p>
                 </div>
                 <div id="course-progress-bar">
-                    <div></div>
+                    <div style={{ width: (stats.complete / stats.total).toFixed(3) + "%" || "50%" }}></div>
                 </div>
             </div>
         </div>
