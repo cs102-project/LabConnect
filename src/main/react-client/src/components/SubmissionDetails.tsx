7bcbce1c45c0bf7777eb180cb527510d826a7d20
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-05-06 07:03:50 +0300

    Fixed SubmissionDetails screen

--- src/main/react-client/src/components/SubmissionDetails.tsx
+++ src/main/react-client/src/components/SubmissionDetails.tsx
@@ -7,12 +7,12 @@ import PageHeader from "./PageHeader";
 // This page shows a list of attempts made to this assignment (the attempts of this submission)
 
 function SubmissionDetails(): JSX.Element {
-    
+
     const params = useParams<{ assignmentid: string, submissionid: string }>();
-    
+
     const [assignment, setAssignment] = useState<IAssignment>();
     const [submission, setSubmission] = useState<ISubmission>();
-    
+
     useEffect(() => {
         APITools.getSubmissionOfStudentFor(params.assignmentid).then((response) => {
             if (APITools.helpers.isSubmissionValid(response)) {
@@ -23,38 +23,41 @@ function SubmissionDetails(): JSX.Element {
             setAssignment(response);
         });
     }, []);
-    
+
     return (
         <div id="submission-details-container">
             <PageHeader pageName="Submission Details" />
-            
+
             <main id="submission-details-panel">
-                
+
                 <h3>{assignment?.title}</h3> {/* temporary */}
-                
+
                 <AddAttemptButton assignmentid={params.assignmentid} />
-                
+
+                {/*
                 <div id="submission-attempts-list-header">
                     <span>Attempt Identity</span>
                     <span>Grade</span>
                     <span>Test Success Rate</span>
                     <span>Go to Attempt</span>
                 </div>
-                
+                */}
+
                 {submission?.attempts.slice().sort((a, b) => b.id - a.id).map((attempt, i) => {
                     return (<article key={i}>
-                        <section>#{attempt.id}: {attempt.attemptFilename}</section>
-                        <section>{attempt.feedback?.grade || "N/A"}</section>
-                        <section>{attempt.testResults.filter(testResult => testResult.state === "SUCCESS").length} / {attempt.testResults.length}</section>
-                        <section><Link to={`/assignments/${params.assignmentid}/submissions/${params.submissionid}/attempts/${attempt.id}`}><span className="material-icons">find_in_page</span></Link></section>
+                        <section><span><br /></span></section>
+                        <section><span>Attempt Identity: </span>#{attempt.id}: {attempt.attemptFilename}</section>
+                        <section><span>Grade: </span>{attempt.feedback?.grade || "N/A"}</section>
+                        <section><span>Test Success Rate: </span>{attempt.testResults.filter(testResult => testResult.state === "SUCCESS").length} / {attempt.testResults.length}</section>
+                        <section><span>Go to Attempt: </span><Link to={`/assignments/${params.assignmentid}/submissions/${params.submissionid}/attempts/${attempt.id}`}><span className="material-icons">find_in_page</span></Link></section>
                     </article>)
                 })}
-                
+
             </main>
-            
+
         </div>
     );
-    
+
 }
 
 export default SubmissionDetails;
